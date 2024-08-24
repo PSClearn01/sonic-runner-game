90ba7ce2ee14d21914fe459c2b8fdf1cc0af4dcb
PuroSurCCD <purosurbxo@gmail.com>
2024-08-24 01:36:34 -0700

    final commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import k from "./kaplayCtx";
+import game from "./scenes/game";
+import gameover from "./scenes/gameover";
 import mainMenu from "./scenes/mainMenu";
 
 k.loadSprite("chemical-bg", "graphics/chemical-bg.png");
@@ -36,8 +38,8 @@ k.loadSound("city", "sounds/city.mp3");
 
 k.scene("main-menu", mainMenu);
 
-k.scene("game", () => {});
+k.scene("game", game);
 
-k.scene("gameover", () => {});
+k.scene("gameover", gameover);
 
 k.go("main-menu");
